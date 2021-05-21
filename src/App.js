3b3e4823fbcde2cbae4593fb47953106f82e2ccc
Jieun Yu <55225394+jieunyu0623@users.noreply.github.com>
2021-05-20 19:25:44 -0700

    Add files via upload
    github finder project

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Fragment } from 'react';
+import Navbar from './components/layout/Navbar';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Users from './components/users/Users';
+import User from './components/users/User';
+import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
+import About from './components/pages/About';
+
+import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
+
+const App = () => {
+	return (
+		<GithubState>
+			<AlertState>
+				<Router>
+					<div className='App'>
+						<Navbar />
+						<div className='container'>
+							<Alert />
+							<Switch>
+								<Route
+									exact
+									path='/'
+									render={(props) => (
+										<Fragment>
+											<Search />
+											<Users />
+										</Fragment>
+									)}
+								/>
+								<Route exact path='/about' component={About} />
+								<Route exact path='/user/:login' component={User} />
+							</Switch>
+						</div>
+					</div>
+				</Router>
+			</AlertState>
+		</GithubState>
+	);
+};
+
+export default App;
---
+++ src/components/repos/Repos.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import RepoItem from './RepoItem';
+
+
+const Repos = ({ repos }) => {
+    return (<div>
+        {repos.map(repo => (<RepoItem key={repo.id} repo={repo} />))}
+    </div>)
+}
+
+Repos.propTypes = {
+    repos: PropTypes.array.isRequired,
+}
+
+export default Repos
